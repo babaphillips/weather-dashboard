59804fb785b1181be0d314f201707b848bb4a17a
babaphillips <amandavphillips@icloud.com>
2022-07-17 17:23:02 -0400

    initial html

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,23 +1,26 @@
-var oneCall = "https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}"
-var getCoordURL ='http://api.openweathermap.org/geo/1.0/direct?q={city name}&appid={API key}'
-var location = document.getElementById('location')
-var submitBtn = document.getElementById('location-search')
+//var oneCall = "https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}"
+//var getCoordURL ='http://api.openweathermap.org/geo/1.0/direct?q={city name}&appid={API key}'
+//var location = document.getElementById('location')
+//var submitBtn = document.getElementById('location-search')
 
 // create a function that captures the users input
-function captureUserInput(event) {
-event.preventDefault();
+///function captureUserInput(event) {
+//event.preventDefault();
 
-var userInput =  location.value;
-getCoords(userInput)
+//var userInput =  location.value;
+//getCoords(userInput)
 
-}
+//}
 // create a function that recieves the users city and runs the getCoord endpoint
-function getCoords(city){
-console.log(city)
-}
+//function getCoords(city){
+//console.log(city)
+//}
 // pass the content needed for the onecall api to retreive all data needed for the html
 
 
 
 
-submitBtn.addEventListener('submit', captureUserInput)
+//submitBtn.addEventListener('submit', captureUserInput)
+
+
+//  This is where all global variable should be declared.
